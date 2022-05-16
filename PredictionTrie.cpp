e78312fa93547bdf49c34b963df2961c06ed2a6c
Ilya Bezrukov <bezrukoff888@gmail.com>
2022-05-17 02:11:50 +0300

    Initial commit

---
+++ PredictionTrie.cpp
@@ -0,0 +1,120 @@
+#include "PredictionTrie.h"
+
+#include <algorithm>
+
+PredictionTrie::PredictionTrie()
+{
+    _root = new PredictionTrie::PredictionTrieNode;
+    _root->count = 0u;
+    _root->type = PredictionTrie::PredictionTrieNode::Type::Root;
+}
+
+PredictionTrie::~PredictionTrie()
+{
+
+}
+
+void PredictionTrie::insert(const std::string& word)
+{
+    auto* current = _root;
+    for (auto letter : word)
+    {
+        auto foundIt = current->children.find(letter);
+        if (foundIt == current->children.end())
+        {
+            auto [it, _] = current->children.emplace(letter, new PredictionTrieNode);
+            foundIt = it;
+        }
+
+        current = foundIt->second;
+    }
+
+    current->type = PredictionTrie::PredictionTrieNode::Type::Leaf;
+    current->count += 1u;
+}
+
+void PredictionTrie::remove(const std::string& word)
+{
+
+}
+
+const PredictionTrie::PredictionTrieNode* PredictionTrie::find(const std::string& word) const
+{
+    auto* current = _root;
+    for (auto letter : word)
+    {
+        auto foundIt = current->children.find(letter);
+
+        if (foundIt == current->children.end())
+        {
+            return nullptr;
+        }
+
+        current = foundIt->second;
+    }
+
+    return current;
+}
+
+bool PredictionTrie::isPresented(const std::string& word) const
+{
+    auto* found = find(word);
+
+    return found && found->type == PredictionTrie::PredictionTrieNode::Type::Leaf;
+}
+
+std::vector<PredictionTrie::MatchedPair> PredictionTrie::allWordsStartedWith(const std::string& wordBegin) const
+{
+    auto* found = find(wordBegin);
+    if (!found)
+    {
+        return {};
+    }
+
+    std::vector<MatchedPair> result;
+    collectAllWordsStartedWith(wordBegin, found->children, result);
+
+    return result;
+}
+
+void PredictionTrie::collectAllWordsStartedWith(
+    const std::string& wordPart,
+    const std::unordered_map<char, PredictionTrieNode*>& letterLayer,
+    std::vector<PredictionTrie::MatchedPair>& result) const
+{
+    for (auto&& [letter, node] : letterLayer)
+    {
+        std::string word = wordPart + letter;
+        if (node->type == PredictionTrie::PredictionTrieNode::Type::Leaf)
+        {
+            result.push_back(std::make_tuple(word, node->count));
+        }
+
+        collectAllWordsStartedWith(word, node->children, result);
+    }
+}
+
+std::string PredictionTrie::findBestMatch(const std::string& wordBegin) const
+{
+    return findBestMatches(wordBegin, 1).front();
+}
+
+std::vector<std::string> PredictionTrie::findBestMatches(const std::string& wordBegin, unsigned count) const
+{
+    auto allWords = allWordsStartedWith(wordBegin);
+
+    count = allWords.size() < count ? allWords.size() : count;
+
+    std::partial_sort(
+        std::begin(allWords),
+        allWords.begin() + count,
+        std::end(allWords),
+        [](const auto& first, const auto& second) {
+            return std::get<1>(first) > std::get<1>(second);
+        }
+    );
+
+    std::vector<std::string> result(allWords.begin(), allWords.begin() + count);
+
+    return result;
+}
---
+++ PredictionTrie.h
@@ -0,0 +1,53 @@
+#ifndef PREDICTIONTRIE_H
+#define PREDICTIONTRIE_H
+
+#include <unordered_map>
+#include <tuple>
+#include <vector>
+#include <string>
+
+class PredictionTrie
+{
+    using MatchedPair = std::tuple<std::string, unsigned int>;
+
+    struct PredictionTrieNode
+    {
+        enum class Type
+        {
+            Root,
+            Regular,
+            Leaf,
+        };
+
+        Type type = Type::Regular;
+        std::unordered_map<char, PredictionTrieNode*> children;
+        unsigned int count = 0;
+    };
+
+    PredictionTrieNode* _root;
+
+    const PredictionTrieNode* find(const std::string& word) const;
+
+    void collectAllWordsStartedWith(
+        const std::string& wordPart,
+        const std::unordered_map<char, PredictionTrieNode*>& letterLayer,
+        std::vector<MatchedPair>& result) const;
+
+public:
+    PredictionTrie();
+    ~PredictionTrie();
+
+    PredictionTrie(const PredictionTrie& other) = delete;
+    PredictionTrie(PredictionTrie&& other) = delete;
+
+    void insert(const std::string& word);
+    void remove(const std::string& word);
+
+    bool isPresented(const std::string& word) const;
+
+    std::vector<MatchedPair> allWordsStartedWith(const std::string& wordBegin) const;
+    std::string findBestMatch(const std::string& wordBegin) const;
+    std::vector<std::string> findBestMatches(const std::string& wordBegin, unsigned count) const;
+};
+
+#endif // PREDICTIONTRIE_H
